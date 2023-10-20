da8b6cfa0fe45568f125cb056a2d73ce48303de3
nolan <presidentvalentineSBR@example.com>
2023-10-20 18:21:04 -0400

    Added a way to load the training data reps into yaml

--- data_representation_abstracted copy 2.py
+++ data_representation_abstracted copy 2.py
@@ -4,6 +4,7 @@ from preprocessing import Preprocess
 import cv2
 from skimage.filters import threshold_otsu
 import csv
+import yaml
 
 from queue import *
 from threading import Thread
@@ -39,7 +40,7 @@ def represent_training(training_labels: dict, parameters: dict):
                 break
             data = thing[1]
             results = represent_data(data['image'], parameters)
-            r_queue.put((data['label'], results))
+            r_queue.put([data['label'], results])
             d_queue.task_done()
 
             print("representing {0}...".format(thing[0]))
@@ -47,7 +48,7 @@ def represent_training(training_labels: dict, parameters: dict):
 
     for PID in PIDs:
         d_queue.put((PID, training_labels[PID]))
-    for _ in range(num_threads):
+    for n_thread in range(num_threads):
         d_queue.put(None)
 
     threads = []
@@ -91,7 +92,7 @@ def represent_data(image, parameters: dict = {}):
         while len(completed) > parameters['optimal_size']:
             completed.pop()
 
-        return np.array(completed)
+        return completed
 
 
         
@@ -102,59 +103,17 @@ def represent_data(image, parameters: dict = {}):
     # 2) convert to histogram array
     sums = np.sum(processed_image, axis=0)
     reg_sum = sums / max(sums)
-    feature_vector = regularize(remove_white(reg_sum))
+    feature_vector_p = regularize(remove_white(reg_sum))
 
-    # 3) smooth?    
+    # 3) smooth?
+    for num in feature_vector_p:
+        feature_vector.append(float(num))
     return feature_vector
 
+
 training_data = process_training(load_training("data", "data\\15021026 1.csv"))
-import pdb; pdb.set_trace()
 represented_training = represent_training(training_data, parameters={'optimal_size': training_data[list(training_data.keys())[0]]['image'].shape[1]})
-pass
+with open("trained_reps.yaml", "w") as file:
+    yaml_file = yaml.safe_dump(represented_training, file)
 
-"""
-threshold = 180
-for i in range(len(DELL_type1_images)):
-    DELL_type1_images[i] = 255 - cv2.cvtColor(remove(DELL_type1_images[i]), cv2.COLOR_RGB2GRAY)
-    DELL_type1_images[i] = np.where(DELL_type1_images[i] < threshold, 0, 1)
-    for k in range(DELL_type1_images[i].shape[1]):
-        DELL_type1_images_codes[i].append(np.sum(DELL_type1_images[i][:, k]))
-    DELL_type1_images_codes[i] /= np.max(DELL_type1_images_codes[i])
-
-
-
-for i in range(len(SEAGATE_images)):
-    SEAGATE_images[i] = 255 - cv2.cvtColor(remove(SEAGATE_images[i]), cv2.COLOR_RGB2GRAY)
-    SEAGATE_images[i] = np.where(SEAGATE_images[i] < threshold, 0, 1)
-    for k in range(SEAGATE_images[i].shape[1]):
-        SEAGATE_images_codes[i].append(np.sum(SEAGATE_images[i][:, k]))
-    SEAGATE_images_codes[i] /= np.max(SEAGATE_images_codes[i])
-
-images = [DELL_type1_images, SEAGATE_images]
-image_codes = [DELL_type1_images_codes, SEAGATE_images_codes]
-rows = len(images)
-
-columns = len(image_codes[0])
-fig, ax = plt.subplots(rows, columns, sharex='col', sharey='row')
-
-for row in range(rows):
-    for col in range(columns):
-        btm_labels = np.arange(len(image_codes[row][col]))
-        ax[row,col].bar(btm_labels, image_codes[row][col])
-
-plt.show()
-
-avg_curves = []
-for code_list in range(len(image_codes)):
-    avg_curves.append(np.sum(image_codes[code_list], axis=0)/len(image_codes[code_list][0]))
-
-rows = len(images)
-fig, ax = plt.subplots(rows, 1, sharex='col', sharey='row')
-for row in range(rows):
-    btm_labels = np.arange(len(avg_curves[row]))
-    ax[row].bar(btm_labels, avg_curves[row])
-
-plt.show()
-
-pass
-"""
+print("SUCCESS!")
