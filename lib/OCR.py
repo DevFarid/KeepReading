f4e584fd72c014f51a0c2d69cb45ea26784d379f
DevFarid <faridkamizi@gmail.com>
2023-11-20 16:25:13 -0500

    refactor: add config settings to OCR reader

--- lib/OCR.py
+++ lib/OCR.py
@@ -25,6 +25,14 @@ class ObjectCharacterRecognition:
 
 	@staticmethod
 	def getResults(image):
+		"""The result of an image run through OCR with OCR settings of returning a dictionary of results.
+
+		Args:
+			image (_type_): _description_
+
+		Returns:
+			_type_: _description_
+		"""
 		# rgb = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
 		# plt.imshow(image)
 		# plt.show()
@@ -32,61 +40,43 @@ class ObjectCharacterRecognition:
 		return results
 
 	@staticmethod
-	def read(image, min_conf=0):
-		# We load the input image and then convert
-		# it to RGB from BGR. We then use Tesseract
-		# to localize each area of text in the input
-		# image
+	def read(image, min_conf=0, psm=3, oem=3):
+		# Convert image to RGB
 		rgb = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
-		results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
 
-		#store list of text read and their confidence level
+		# PyTesseract settings
+		custom_config = r'--oem {} --psm {}'.format(oem, psm)
+
+		# Use Tesseract to localize each area of text in the input image
+		results = pytesseract.image_to_data(rgb, config=custom_config, output_type=Output.DICT)
+
+		# Initialize lists to store text and confidence levels
 		list_text = []
 		list_lv = []
-		# Then loop over each of the individual text
-		# localizations
+
+		# Process each text localization
 		for i in range(0, len(results["text"])):
-			
-			# We can then extract the bounding box coordinates
-			# of the text region from the current result
 			x = results["left"][i]
 			y = results["top"][i]
 			w = results["width"][i]
 			h = results["height"][i]
-			
-			# We will also extract the OCR text itself along
-			# with the confidence of the text localization
+
 			text = results["text"][i]
 			conf = int(results["conf"][i])
 
-			# filter out weak confidence text localizations
+			# Filter out weak confidence text localizations
 			if conf > min_conf:
-				
-				# We will display the confidence and text to
-				# our terminal
 				print("Confidence: {}".format(conf))
 				print("Text: {}".format(text))
 				print("")
-				
-				list_text = list_text.append(text)
-				list_lv = list_lv.append(conf)
-				# We then strip out non-ASCII text so we can
-				# draw the text on the image We will be using
-				# OpenCV, then draw a bounding box around the
-				# text along with the text itself
+
+				list_text.append(text)
+				list_lv.append(conf)
+
+				# Draw bounding box and text
 				text = "".join(text).strip()
-				cv2.rectangle(image,
-							(x, y),
-							(x + w, y + h),
-							(0, 0, 255), 2)
-				cv2.putText(image,
-							text,
-							(x, y - 10),
-							cv2.FONT_HERSHEY_SIMPLEX,
-							1.2, (0, 255, 255), 3)
-		# return a tuple that contained image, text list and confidence list
-		data = tuple((image,list_text,list_lv))
-		return data
-		# After all, we will show the output image
-		# cv2.imshow("Image", image)
-		# cv2.waitKey(0)
+				cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+				cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 255), 3)
+
+		# Return a tuple that contains image, text list, and confidence list
+		return image, list_text, list_lv
